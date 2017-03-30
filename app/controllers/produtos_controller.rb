eeedc3546ecc4a15543a963e58a8e7492a41ad8f
Julio Cezar <juliogama@live.com>
2017-03-30 18:39:16 +0000

    Add create produto functionality

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,12 +1,31 @@
 class ProdutosController < ApplicationController
+  before_action :set_produto, only: [:destroy]
+  
   def index
     @produtos_por_nome = Produto.order(:nome).limit 9
     @produtos_por_preco = Produto.order(:preco).limit 2
   end
   
+  def new
+    @produto = Produto.new
+  end
+  
   def create
-    valores = params.require(:produto).permit(:nome,:descricao,:quantidade,:preco)
-    Produto.create valores
+    @produto = Produto.create produto_params
+    redirect_to root_path
+  end
+  
+  def destroy
+    @produto.destroy
+    redirect_to root_path
+  end
+  
+  def set_produto
+    @produto = Produto.find(params[:id])
+  end
+  
+  def produto_params
+    params.require(:produto).permit(:nome,:descricao,:quantidade,:preco)
   end
   
 end
